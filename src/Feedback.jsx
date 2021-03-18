bc8612bf16098a0ffac2208526e70f26a4ba1f74
Dmitry Tsyplov <d.tsyplov@mail.ru>
2021-03-18 06:54:48 +0200

    контролируемые формы

--- src/Feedback.jsx
+++ src/Feedback.jsx
@@ -1,30 +1,54 @@
 import React from 'react'
+import {useState} from 'react';
+
+
 
 
 function Feedback(props) {
+
+    const [form, setForm] = useState({
+        name: 'БИБА',
+        score: 'good',
+        agreement: false
+    });
+    
     const handleSubmit = evt => {
         evt.preventDefault();
         console.log(evt.type)
         console.log(evt.target)
     }
+
+    const handleChange = ({target}) =>{
+        const name = target.name;
+        const value = target.type ==='checkbox' ? target.checked : target.value;
+        setForm(prevForm=> ({...prevForm, [name]: value}))
+    }
+
+    const handleNameChange = evt => {
+        setForm(prevForm=> ({...prevForm, name: evt.target.value}))
+    }
+
     return (
         <div>
             <form onSubmit={handleSubmit}>
                 <div>
-                    <label htmlFor='name'></label>
-                    <input id='name' name='name'></input>
+                    <label htmlFor='name'>Ваше имя</label>
+                    <input id='name' name='name' value={form.name} onChange={handleNameChange}></input>
                 </div>
                 <div>
-                    <label htmlFor='satisfaction'></label>
-                    <select name="satisfaction" id="satisfaction">
-                        <option value="good">Хорошо</option>
-                        <option value="bad">Плохо</option>
+                    <label htmlFor='score'>Выберите уровень удовлетворённости</label>
+                    <select name="score" id="score" value={form.score} onChange={handleChange}>
+                        <option value="good">good</option>
+                        <option value="Normal">Normal</option>
+                        <option value="bad">Bad</option>
                     </select>
                 </div>
                 <div>
                     <label htmlFor="agreement">
-                        <input type="checkbox" name='agreement' id='agreement'/>
+                        <input  name='agreement' id='agreement' type='checkbox'
+                        checked={form.agreement} onChange={handleChange}/>
                           Согласен на всё
+                       
                     </label>
                     <button type='submit'>Отправить</button>
                     </div>
